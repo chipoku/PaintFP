723e5504fd797d65812e778d05a50f9fc6840bf1
chipoku <slakoth.ramtos6@gmail.com>
2021-06-21 17:11:51 +0300

    beta v1.1.1 patch for project WARNING: VERY BAD CODE! fixed some things & code became a little better

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,27 @@
 #include <iostream>
 
 #define KEY_SPACE 32
-#define KEY_BACKSPACE 127
+#define KEY_BACKSPACE 127 // default KEY_BACKSPACE not working with ncurses.
 
 class PaintFP {
 private:
-    int x = 1, y = 1;
-    const int min_x = 0, min_y = 0;
-    const int width = 100, height = 100;
+    int x = 1,
+        y = 1;
+    int &width = COLS,
+        &height = LINES;
+    /*
+     * int hw_width = static_cast<int>(NULL),
+     *     hw_height = static_cast<int>(NULL);
+     */
 
     WINDOW *help_win = nullptr;
-    const char help_msg[150] = "1. UP,DOWN,LEFT,RIGHT - arrows\n"
-                               " 2. SPACE - draw\n"
-                               " 3. BACKSPACE - erasure\n"
-                               " 4. h - help\n"
-                               " 5. r - clear all\n"
-                               " 6. q - quit\n";
+
+    const char help_msg[117] = "1. UP,DOWN,LEFT,RIGHT - arrows\n"
+                               " 2. DRAW - SPACE\n"
+                               " 3. ERASURE - BACKSPACE\n"
+                               " 4. HELP - h\n"
+                               " 5. CLEAR ALL - r\n"
+                               " 6. QUIT - q\n";
 public:
     int ch = static_cast<int>(NULL);
 
@@ -24,25 +30,40 @@ public:
     PaintFP() {
         initscr();
         start_color();
-        init_colors(COLOR_BLACK, COLOR_WHITE);
-//        attron(COLOR_PAIR(1));
+        init_colors(COLOR_BLACK,
+                    COLOR_WHITE);
         noecho();
         nodelay(stdscr, TRUE);
     }
+
     static void init_colors(short font, short background) {
-        init_pair(1, font, background);
+        init_pair(1, font,
+                     background);
     }
     void help_menu(){
-        help_win = newwin(LINES/2, COLS/2, LINES/4, COLS/4);
-        mvwprintw(help_win, 1, COLS/8, "Print For Paupers v1.1b - HELP");
-        mvwaddstr(help_win, 4, 1, help_msg);
-        box(help_win, 0, 0);
+        /*
+        hw_height = LINES/2,
+        hw_width = COLS/2;
+        */
+        help_win = newwin(height/2, width/2,
+                          LINES/4, COLS/4);
+
+        mvwprintw(help_win,
+                  1, width/8,
+                  "Print For Paupers v1.1.1b - HELP:");
+        mvwaddstr(help_win,
+                4, 1,
+                  help_msg);
+        box(help_win,
+                0, 0);
+
         wrefresh(help_win);
     }
-
     void check_pos() const {
         attron(COLOR_PAIR(1));
-        mvprintw(0, 0, "PaintFP v1.1b - X: %i Y: %i, COLS: %i, LINES: %i", x, y, COLS, LINES);
+        mvprintw(0, 0,
+                 "PaintFP v1.1.1 - X: %i Y: %i, WIDTH: %i, HEIGHT: %i",
+                 x, y, width, height);
         attroff(COLOR_PAIR(1));
     }
     void move_cur() {
@@ -56,8 +77,8 @@ public:
             x -= 1;
         move(y, x);
     }
-     void draw() const {
-        if (x < width && y < height && x > min_x && y > min_y) {
+    void draw() const {
+        if (x < width && y < height && x > 0 && y > 0) {
             if (ch == KEY_SPACE)
                 addch('#');
             if (ch == KEY_BACKSPACE)
@@ -68,7 +89,6 @@ public:
     }
 
     ~PaintFP() {
-//        attroff(COLOR_PAIR(1));
         endwin();
     }
 };
@@ -77,17 +97,17 @@ int main() {
 PaintFP game;
 
     while (true){
-     game.ch = getch();
+        game.ch = getch();
 
-     game.check_pos();
-     game.move_cur();
-     game.draw();
+        game.check_pos();
+        game.move_cur();
+        game.draw();
 
-     if (game.ch == 'h')
-         game.help_menu();
-     if (game.ch == 'q')
-         break;
-     }
+        if (game.ch == 'h')
+             game.help_menu();
+        if (game.ch == 'q')
+             break;
+    }
 
     endwin();
     return 0;
